f2307d6e72a2695417345733507e712f8abd1be6
Sena Gamsizoglu <gamsizsenaa@gmail.com>
2022-05-02 02:18:31 +0200

    Add di implementation

--- app/src/main/java/com/example/realestateapp/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/realestateapp/data/local/database/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.realestateapp.data.local.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.realestateapp.data.local.dao.HouseDao
 import com.example.realestateapp.data.model.House
@@ -10,23 +8,4 @@ import com.example.realestateapp.data.model.House
 @Database(entities = [House::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun houseDao(): HouseDao
-
-    companion object {
-
-        // For Singleton instantiation
-        @Volatile
-        private var instance: AppDatabase? = null
-        private const val DATABASE_NAME = "house_database"
-
-        fun getInstance(context: Context): AppDatabase {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
-
-        private fun buildDatabase(context: Context): AppDatabase {
-            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-                .build()
-        }
-    }
 }
